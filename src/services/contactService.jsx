ce5f7dac5c168c6f998841b1e2eadcb0ca781851
Santoshi <srbaslingol@gmail.com>
2026-03-16 10:23:26 +0530

    force rebuild with new API

--- src/services/contactService.jsx
+++ src/services/contactService.jsx
@@ -2,12 +2,7 @@ import axios from "axios";
 
 const API_URL = "https://mern-contact-backend-w929.onrender.com/api/contact";
 
-export const sendContact = async (data) =>{
-    try{
-        const response = await axios.post(API_URL,data);
-        return response.data;
-    } catch (error){
-        console.error(error);
-    }
+export const sendContact = async (data) => {
+  return axios.post(API_URL, data);
 };
 
