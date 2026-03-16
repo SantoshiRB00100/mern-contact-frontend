f762c086e4cdc368fe4f74aa5f296a7beef48551
Santoshi <srbaslingol@gmail.com>
2026-03-16 11:08:27 +0530

    fixed api url

--- src/services/contactService.jsx
+++ src/services/contactService.jsx
@@ -2,7 +2,11 @@ import axios from "axios";
 
 const API_URL = "https://mern-contact-backend-w929.onrender.com/api/contact";
 
-export const sendContact = async (data) =>{
-    const response = await axios.post(API_URL,data);
+export const sendContact = async (data) => {
+  try {
+    const response = await axios.post(API_URL, data);
     return response.data;
+  } catch (error) {
+    console.error(error);
+  }
 };
